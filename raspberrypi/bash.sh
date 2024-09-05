d4f6534569c317bd74595beb476d7e9b5e058a7a
kli63 <kli63@ThisIsLaptop>
2024-09-05 14:14:41 -0400

    Update script

--- raspberrypi/bash.sh
+++ raspberrypi/bash.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 IP=$(hostname -I | awk '{print $1}')
+
+FILE_PATH="$HOME/334/raspberrypi/ip.md"
+
+echo $IP > $FILE_PATH
+
+cd "$HOME/334/raspberrypi" || exit
+
+git add ip.md
+
+git commit -m "Update IP Address"
+
+git push origin main
