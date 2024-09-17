3a55ceaaabaeaa356fbee831e3ca7f2380275c84
kli63 <kli131163@gmail.com>
2024-09-17 02:41:11 -0400

    working runtime script v1.0

--- Project1/play.sh
+++ Project1/play.sh
@@ -3,6 +3,14 @@ sudo fuser -k 8000/tcp
 
 python3 -m http.server 8000 &
 
-sleep 2
+if [ -z "$DISPLAY" ]; then
+  export DISPLAY=:0
+fi
 
-chromium-browser --kiosk --incognito --enable-logging=stderr --v=1 http://localhost:8000
+chromium-browser --kiosk --incognito --disable-restore-session-state --enable-logging=stderr --v=1 http://localhost:8000 &
+
+# sleep 5
+
+xdotool search --onlyvisible --class "chromium" windowactivate --sync key F11
+
+wmctrl -r Chromium -b add,fullscreen
