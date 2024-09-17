e1257c6d0f53f4836567a10f25e4409b55cb5d76
kli63 <kli63@ThisIsLaptop>
2024-09-17 00:13:10 -0400

    test script changes

--- Project1/play.sh
+++ Project1/play.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+sudo apt-get install wmctrl
+
+xset s off
+xset -dpms
+
 sudo fuser -k 8000/tcp
 
 python3 -m http.server 8000 &
 
 sleep 2
 
-chromium-browser --kiosk --incognito --enable-logging=stderr --v=1 http://localhost:8000
+if [ -z "$DISPLAY" ]; then
+    startx
+fi
+
+chromium-browser --kiosk --start-fullscreen --disable-gpu --incognito http://localhost:8000 &
+
+sleep 5
+
+wmctrl -r Chromium -b add,fullscreen
