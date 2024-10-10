877fe14b77460104ed53d1b06885d1f0506ae76f
kli63 <kli131163@gmail.com>
2024-10-10 16:21:01 -0400

    part of lab 6

--- Labs/Lab6/server.py
+++ Labs/Lab6/server.py
@@ -8,10 +8,10 @@ s.listen(0)
 while True:
         client, addr = s.accept()
         while True:
-                content = client.recv(32)
+                content = client.recv(32).decode("utf-8")
                 if len(content) == 0:
                         break
                 else:
                         print(content)
-        print("Closing connection")
+        # print("Closing connection")
         client.close()
